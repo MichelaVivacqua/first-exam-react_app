dce79b4595f3c34ebd3ae8757b6ffb1e408b3ab3
MichelaVivacqua <147068184+MichelaVivacqua@users.noreply.github.com>
2024-02-02 13:12:07 +0100

    first row done

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 import MyNavbar from "./components/MyNavbar";
 import MyFooter from "./components/MyFooter";
 import TVShows from "./components/TvShows";
+import Content from "./components/Content";
 
 function App() {
   return (
@@ -13,6 +14,9 @@ function App() {
       </header>
       <main>
         <TVShows />
+        <h4>Trending Now</h4>
+        {/* <div className="row row-cols-1 row-cols-sm-2 row-cols-lg-4 row-cols-xl-6 mb-4"> */}
+        <Content />
       </main>
       <MyFooter />
     </>
---
+++ src/components/Content.jsx
@@ -0,0 +1,49 @@
+import { Component } from "react";
+
+class Content extends Component {
+  state = {
+    arrayOfFilms: [],
+  };
+
+  componentDidMount() {
+    this.fetchFilms();
+  }
+
+  fetchFilms = () => {
+    fetch("http://www.omdbapi.com/?apikey=8d3cdf1b&s=007")
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error("Errore nel recupero della tua saga");
+        }
+      })
+      .then((data) => {
+        this.setState({ arrayOfFilms: data.Search || [] });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  render() {
+    const { arrayOfFilms } = this.state;
+
+    return (
+      <div className="d-flex flex-nowrap overflow-auto">
+        {arrayOfFilms.map((film) => (
+          <div
+            key={film.imdbID}
+            className="col-12 col-md-2 mb-2 text-center px-1"
+          >
+            <img
+              src={film.Poster}
+              alt={film.Title}
+              style={{ width: "120px" }}
+            />
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+export default Content;
